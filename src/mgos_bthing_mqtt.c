08c5863c20ec00daf7a309387ede7a99ce822a82
fbalestrini <gaojing@hotmail.it>
2021-06-10 09:31:25 +0200

    Updated

--- src/mgos_bthing_mqtt.c
+++ src/mgos_bthing_mqtt.c
@@ -66,10 +66,8 @@ void mg_bthing_mqtt_on_set_state(struct mg_connection *nc, const char *topic,
   if (!msg || !item || !item->enabled) return;
 
   mgos_bvar_t state;
-  if (!mgos_bvar_json_try_bscanf(msg, msg_len, &state)) {
+  if (!mgos_bvar_json_try_bscanf(msg, msg_len, &state))
     state = mgos_bvar_new_nstr(msg, msg_len);
-    LOG(LL_INFO, ("NOT JSON: %s(type %d, len %d)", (state ? "NOT NULL" : "NULL"), mgos_bvar_get_type(state), msg_len));
-  }
 
   if (s_mqtt_sub_mode == MG_BTHING_MQTT_MODE_SINGLE) {
     mgos_bthing_set_state(item->thing, state);

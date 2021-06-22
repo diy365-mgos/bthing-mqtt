f9939252ca5c191597e2f2c6126fd3656c5ee87e
fbalestrini <gaojing@hotmail.it>
2021-06-22 12:50:49 +0200

    Updated

--- src/mgos_bthing_mqtt.c
+++ src/mgos_bthing_mqtt.c
@@ -146,12 +146,10 @@ static void mg_bthing_mqtt_on_state_changed(int ev, void *ev_data, void *userdat
   if (!mgos_mqtt_global_is_connected()) return;
   
   #ifdef MGOS_BTHING_HAVE_SHADOW
-  LOG(LL_INFO, ("Publishig the shadow..."));
   struct mgos_bthing_shadow_state *state = (struct mgos_bthing_shadow_state *)ev_data;
   if (!mg_bthing_mqtt_pub_state(mgos_sys_config_get_bthing_mqtt_pub_topic(), state->full_shadow)) {
     LOG(LL_ERROR, ("Error publishing '%s' shadow.", mgos_sys_config_get_device_id()));
   }
-  LOG(LL_INFO, ("Shadow published."));
 
   #else
   mgos_bthing_t thing = (mgos_bthing_t)ev_data;
